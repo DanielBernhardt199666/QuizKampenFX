939a51a88bcea2f94ce016d8ca8668e981bcf2bd
DanielBernhardt199666 <55973975+DanielBernhardt199666@users.noreply.github.com>
2019-11-20 14:07:30 +0100

    Update ChooseSubject.java

--- src/sample/ChooseSubject.java
+++ src/sample/ChooseSubject.java
@@ -1,6 +1,34 @@
 package sample;
 
-public class ChooseSubject {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
-    
+public class ChooseSubject{
+
+    @FXML
+    private Button bluebutton;
+
+    @FXML
+    private Button orangeubtton;
+
+    @FXML
+    private Button redbutton;
+
+    @FXML
+    void bluebutton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void orangebutton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void redbutton(ActionEvent event) {
+
+
+    }
+
 }
